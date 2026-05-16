bd138b179a4a7d5935bb51ab6927d07447c32561
dada <ipr79692@gmail.com>
2026-05-16 19:36:31 +0300

    160526CafeSimulator

---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour, IInteractable
+{
+    public Animator animator;
+    public GameObject InteractionText;
+    
+    public string GetDescription()
+    {
+        Debug.Log("OpenDoor::GetDescription(); -- animator.GetBool(Open):" + animator.GetBool("Open"));
+       if (animator.GetBool("Open") == false) return "Нажмите E";
+       return "132";
+    }
+
+    public void Interact()
+    {
+        Debug.Log("Нажали E");
+        if (animator.GetBool("Open") == false)
+        {
+            animator.SetBool("Open", true); 
+        }
+        else
+        {
+            animator.SetBool("Open", false);   
+        }
+
+    }
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
